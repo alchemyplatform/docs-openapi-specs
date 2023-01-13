979693561b96da704ecb8408977acc7a24eb0989
Bastien <bastien@alchemy.com>
2023-01-13 23:05:40 +0100

    Use Readme API instead of rdme in update script

--- scripts/update.js
+++ scripts/update.js
@@ -1,67 +1,50 @@
 require('dotenv').config();
 
-const fs = require('fs');
-const { exec } = require('node:child_process');
-const { parse } = require('yaml');
-
-// see https://www.npmjs.com/package/rdme#authentication
-const key = process.env.RDME_API_KEY;
-if (!key) {
-  throw new Error('Please set RDME_API_KEY in .env');
-}
-
-// 1. Get file path
-const filePath = process.argv[2];
-
-if (!filePath || !filePath.endsWith('.yaml')) {
-  throw new Error('Please provide a YAML file (.yaml)');
-}
-console.log(`File path => ${filePath}`);
-
-// 2. Read file contents
-const contents = fs.readFileSync(filePath, 'utf-8');
-
-// 3. Parse yaml
-const yaml = parse(contents);
-// console.log(yaml);
+const OASNormalize = require('oas-normalize').default;
+const { Readme } = require('./Readme');
 
 // TODO: lint OpenAPI?
-// TODO: validate OpenAPI? currently fallbacks to rdme command to do this
-
-// 4. Search for x-readme id
-const id = yaml['x-readme']?.id;
-console.log(`Readme id => ${id}`);
-
-if (!id) {
-  throw new Error('No id found - ensure it is set at x-readme.id');
-}
-
-// 5. Get operationId (used in url)
-// 1st path item, 1st operation
-const pathItem = Object.values(yaml.paths)[0];
-const operation = Object.values(pathItem)[0];
-const operationId = operation.operationId;
-
-// 6. Build URL
-const baseUrl = 'https://alchemyenterprisegroup.readme.io/reference';
-const url = `${baseUrl}/${operationId.toLowerCase()}`;
-
-// 7. Run readme command
-const command = `npx rdme openapi ${filePath} --key=${key} --id=${id}`;
-console.log(`Running rdme command..\n`);
-
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
+(async () => {
+  // see https://www.npmjs.com/package/rdme#authentication
+  const key = process.env.RDME_API_KEY;
+  if (!key) {
+    throw new Error('Please set RDME_API_KEY in .env');
   }
 
-  if (stdout) {
-    console.log(`${stdout}`);
-    console.log(url);
+  const filePath = process.argv[2];
+  if (!filePath || !filePath.endsWith('.yaml')) {
+    throw new Error('Please provide a YAML file (.yaml)');
   }
+  console.log(`File path => ${filePath}`);
 
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
+  // 2. Deference and validate spec
+  const oas = new OASNormalize(filePath, {
+    enablePaths: true,
+    colorizeErrors: true,
+  });
+
+  await oas.validate();
+
+  // derefSpec will have replaced refs with imported definitions
+  const derefSpec = await oas.deref();
+  // spec needs to be string to upload to readme
+  const spec = JSON.stringify(derefSpec, null, 2);
+  // console.log(spec);
+
+  // 3. Search for x-readme id
+  const id = derefSpec['x-readme']?.id;
+  if (!id) {
+    throw new Error('No id found at x-readme.id');
   }
-});
+  console.log(`Readme id => ${id}`);
+
+  // 4. Update specification
+  const readme = new Readme(key);
+  await readme.spec.update({ id, spec });
+
+  const found = await readme.spec.find({ id });
+  if (found) {
+    const url = Readme.utils.createUrlFromSlug(found.category?.slug);
+    console.log(`URL => ${url}`);
+  }
+})();
