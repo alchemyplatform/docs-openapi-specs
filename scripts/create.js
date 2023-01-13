abf8dd6e5da3c8f4210855269a126a503b09fcfa
Bastien <bastien@alchemy.com>
2023-01-13 21:31:06 +0100

    Use Readme API instead of command line tool

--- scripts/create.js
+++ scripts/create.js
@@ -1,80 +1,63 @@
 require('dotenv').config();
 
 const fs = require('fs');
-const { spawn } = require('node:child_process');
 const { parse, stringify } = require('yaml');
-
-// see https://www.npmjs.com/package/rdme#authentication
-const key = process.env.RDME_API_KEY;
-if (!key) {
-  throw new Error('Please set RDME_API_KEY in .env');
-}
-
-// 1. Get file path
-const filePath = process.argv[2];
-
-if (!filePath || !filePath.endsWith('.yaml')) {
-  throw new Error('Please provide a YAML file (.yaml)');
-}
-console.log(`File path => ${filePath}`);
-
-// 2. Read file contents
-const contents = fs.readFileSync(filePath, 'utf-8');
-
-// 3. Parse yaml
-const yaml = parse(contents);
-// console.log(yaml);
+const parser = require('@apidevtools/json-schema-ref-parser');
+const { Readme } = require('./Readme');
 
 // TODO: lint OpenAPI?
 // TODO: validate OpenAPI? currently fallbacks to rdme command to do this
 
-// 4. Get operationId (used in url)
-// 1st path item, 1st operation
-const pathItem = Object.values(yaml.paths)[0];
-const operation = Object.values(pathItem)[0];
-const operationId = operation.operationId;
-
-// 5. Build URL
-// TODO: invalid url if spec with same operationId already deployed
-const baseUrl = 'https://alchemyenterprisegroup.readme.io/reference';
-const url = `${baseUrl}/${operationId.toLowerCase()}`;
-
-// 6. Run readme command
-console.log(`Running rdme command..\n`);
-
-const cmd = 'npx';
-const args = ['rdme', 'openapi', filePath, `--key=${key}`];
-
-// option 1 - no access to stdout / use readme API to get latest spec / spec by name https://docs.readme.com/main/reference/getapispecification
-// const opts = { stdio: ['inherit', 'inherit', 'inherit'] };
-// const ps = spawn(cmd, args, opts);
-
-// option 2 - only pipe stdout
-// console.clear() will remove logs above
-const opts = { stdio: ['inherit', 'pipe', 'inherit'] };
-const ps = spawn(cmd, args, opts);
-
-// 7. Get readme id and save in spec
-ps.stdout.on('data', (data) => {
-  console.clear();
-
-  const text = data.toString();
-  console.log(text);
-
-  // search for string
-  // rdme openapi spec.yaml --key=<key> --id=63bf7df1df71eb001007ffe9
-  if (text.includes('.yaml --key=<key> --id=')) {
-    const prefix = '--id=';
-    const index = text.indexOf(prefix);
-
-    const id = text.substring(index + prefix.length).replace('\n', '');
-
-    const updatedYaml = { ...yaml };
-    updatedYaml['x-readme'].id = id;
-    // console.log(updatedYaml);
-    fs.writeFileSync(filePath, stringify(updatedYaml));
-    console.log(` ${filePath} => added id ${id} (x-readme.id)\n`);
-
-    console.log(`Url => ${url}`);
+(async () => {
+  // see https://www.npmjs.com/package/rdme#authentication
+  const key = process.env.RDME_API_KEY;
+  if (!key) {
+    throw new Error('Please set RDME_API_KEY in .env');
   }
-});
+
+  const filePath = process.argv[2];
+
+  if (!filePath || !filePath.endsWith('.yaml')) {
+    throw new Error('Please provide a YAML file (.yaml)');
+  }
+  console.log(`File path => ${filePath}`);
+
+  // 2. Read file contents
+  const contents = fs.readFileSync(filePath, 'utf-8');
+
+  // 3. Parse yaml
+  const yaml = parse(contents);
+  console.log(stringify(yaml));
+
+  // see https://phil.tech/2022/bundling-openapi-with-javascript/
+  // see https://github.com/APIDevTools/json-schema-ref-parser#example
+  // see https://github.com/stoplightio/json-schema-ref-parser
+
+  // alternative - https://github.com/readmeio/oas
+  // https://www.npmjs.com/package/oas-normalize
+  const flatten = await parser.dereference(yaml);
+
+  const spec = JSON.stringify(flatten, null, 2);
+  console.log(spec);
+
+  // 4. Upload specification
+  const readme = new Readme(key);
+  const id = await readme.spec.upload({ spec });
+  console.log(id);
+
+  // Add id to spec!
+  const updatedYaml = { ...yaml };
+  updatedYaml['x-readme'].id = id;
+  // console.log(updatedYaml);
+  fs.writeFileSync(filePath, stringify(updatedYaml));
+  console.log(` ${filePath} => added id ${id} (x-readme.id)\n`);
+
+  // const id = '63c18f5c68b7e4004406888f';
+  const found = await readme.spec.find({ id });
+  console.log(found);
+
+  if (found) {
+    const url = Readme.utils.createUrlFromSlug(found.category?.slug);
+    console.log(url);
+  }
+})();
